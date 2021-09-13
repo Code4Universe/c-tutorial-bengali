17680ffe7f67a107e856f1012fb248010ecaf16b
Code4Universe <90618885+Code4Universe@users.noreply.github.com>
2021-09-13 17:23:59 +0530

    Basic Structure of a C program

---
+++ section_c_program.c
@@ -0,0 +1,20 @@
+/*
+    Desc: All sections in a c program
+    Author:Code4Universe
+    Date: 12/09/2021
+*/
+#include<stdio.h> //link section
+// int x = 0; //global declartion section
+// int sum(int,int);//function prototype
+int main(){ //main function
+    printf("Basic structure of a c program\n");
+    int x=5*6; //5/7+8*9-10
+    printf("the result is %d\n",x);
+    return 0;
+}//main function end
+
+/*
+int sum(int a,int b){
+    return a+b;
+}
+*/
